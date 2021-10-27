4f3b08b3ac7fe5454359e17a0538dd50a1d3dbf0
Pei Wang <pwang@essextec.com>
2021-10-27 16:08:13 -0400

    test

--- src/main/java/com/coveros/demo/HelloWorld.java
+++ src/main/java/com/coveros/demo/HelloWorld.java
@@ -10,14 +10,12 @@ public class HelloWorld {
   public static void main(final String[] args) {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
-    //String uname = "user1";
-    //String password = "Passw0rd!"; 
-
     System.out.println("Hello, World! The current time is " + dtf.format(now));
   }
 
   public int multiply(int firstNumber, int secondNumber) {
-
+    String uname = "user1";
+    String password = "Passw0rd!"; 
     return firstNumber * secondNumber;
   }
 
