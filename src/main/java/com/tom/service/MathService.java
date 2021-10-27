7f7bd0ee1e57572d8f3a987b2d260150ff6b7da8
Pei Wang <pwang@essextec.com>
2021-10-27 15:41:00 -0400

    test

---
+++ src/main/java/com/tom/service/MathService.java
@@ -0,0 +1,14 @@
+package com.tom.service;
+
+public class MathService {
+    public int multiply(int firstNumber, int secondNumber) {
+
+        return firstNumber * secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        Integer myInt = null;
+        Integer yourInt = myInt * 2;
+        return firstNumber - secondNumber;
+    }
+}
